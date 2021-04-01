e1816a50ec6f2d91299551b8ae5e8a956a942207
ivanempireser <79630828+ivanempireser@users.noreply.github.com>
2021-04-01 19:02:27 +0200

    todo

--- index.php
+++ index.php
@@ -2,3 +2,61 @@
 require_once('includes/loader.php');
 cargarHeader("Concesionario");
 ?>
+
+<body style="background-color: #151515;">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col my-auto">
+                <div class="row">
+                    <div class="offset-2 col-8">
+                        <h1 class="naranja">Cómo jugar</h1>
+                        <p class="rem-1-2 fill-white">Es muy fácil ser un nuevo ciudadano de Retrasados RP.<br><br>Podrás encontrar toda
+                            la información sobre cómo
+                            conectarte en nuestro Discord. Es necesario que antes de entrar te leas la Normativa general
+                            del
+                            servidor. También deberás consultar algunas otras si quieres aspirar a trabajos públicos
+                            como
+                            Policía, Médico, Mecánico... ¡La ciudad te espera!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <img src="assets/img/desanta.png" class="img-fluid">
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-3">
+                <img src="assets/img/girl.png" class="img-fluid">
+            </div>
+            <div class="col text-center my-auto">
+                <div class="row">
+                    <div class="col">
+                        <h2 class="naranja">trabajos</h2>
+                        <i class="fas fa-users fill-white fa-7x mt-2"></i>
+                        <p class="fill-white mt-3 rem-0-9">Hay muchos trabajos disponibles para tí.<br>
+                            Desde trabajos simples pero honrados hasta trabajos públicos difíciles de conseguir.
+                        </p>
+                    </div>
+                    <div class="col">
+                        <h2 class="naranja">Propiedades</h2>
+                        <i class="far fa-building fill-white fa-7x mt-2"></i>
+                        <p class="fill-white mt-3 rem-0-9">¿Un piso en el centro de la ciudad?
+                            ¿Una casa de lujo con piscina y jardín?
+                            ¿O una casa alejada del loco mundo de Retrasados RP?<br>
+                            Entra para ver todas las propiedades disponibles
+                        </p>
+                    </div>
+                    <div class="col">
+                        <h2 class="naranja">Vehículos</h2>
+                        <i class="fas fa-car fill-white fa-7x mt-2"></i>
+                        <p class="fill-white mt-3 rem-0-9">Tú decides cómo moverte por la ciudad y sus alrededores.<br>
+                            Tienes cientos de coches a tu disposición.<br>
+                            Elige el coche de tus sueños hoy mismo.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
--- partials/header.php
+++ partials/header.php
@@ -10,32 +10,36 @@ echo'
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/new.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     <link href="style.css" rel="stylesheet"></link>
     <title>' . $title . '</title>
 </head>
 <body>
+    <div class="container-fluid menu">
         <div class="container">
             <div class="row">
-                <div class="col">
-                    <a class="navbar-brand img-fluid mt-3 mb-3" href="/"><img src="assets/img/logo.svg"></a>
+                <div class="col-1">
+                    <a class="navbar-brand mt-3 mb-3" href="/"><img class="img-fluid" src="assets/img/logo.png"></a>
                 </div>
                     ';
                     foreach($links as $url=>$nombre){
                         if($_SERVER['REQUEST_URI'] == $url){$class = " active";}else{$class="";}
                         echo '
 
-                        <div class="col my-auto border align-middle" href="'.$url.'">
-                            <p class="no-p '.$class.'">'.$nombre.'</p>
+                        <div class="col my-auto text-center" href="'.$url.'">
+                            <p class="no-p fill-white'.$class.'">'.$nombre.'</p>
                         </div>';
                     }
                     echo'
-                    <div class="col">
-                        <img src="assets/img/discord.svg" class="img-fluid border">
+                    <div class="col-2 my-auto">
+                        <img src="assets/img/discord.svg" class="img-fluid">
                     </div>
                 </div>
             </div>
+        </div>
         
 ';
 }
